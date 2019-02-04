8b35c514508a761833c2037cf208ba9235935518
Ciyang <qciyang@gmail.com>
2019-02-04 01:20:30 -0800

    initial commit

---
+++ js/stims.js
@@ -0,0 +1,7 @@
+allStims = [
+  {noun: "book", adjective: "thick", fileinit: "bookThick", range:[1,4,6,9]},
+  {noun: "cat", adjective: "fat", fileinit: "catFat", range:[1,4,6,9]},
+  {noun: "straw", adjective: "bent", fileinit: "strawStraight", range:[6,7,8,9]},
+];
+
+allStims = _.shuffle(allStims);
