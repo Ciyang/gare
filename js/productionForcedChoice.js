fac6d131e401153296907fcda5eb90e5a5684341
Ciyang <qciyang@gmail.com>
2019-02-08 08:06:15 -0800

    production forced choice

--- js/productionForcedChoice.js
+++ js/productionForcedChoice.js
@@ -1,24 +1,6 @@
 function make_slides(f) {
   var   slides = {};
 
-  slides.consent = slide({
-     name : "consent",
-     start: function() {
-      exp.startT = Date.now();
-      $("#consent_2").hide();
-      exp.consent_position = 0;
-     },
-    button : function() {
-      if(exp.consent_position == 0) {
-         exp.consent_position++;
-         $("#consent_1").hide();
-         $("#consent_2").show();
-      } else {
-        exp.go(); //use exp.go() if and only if there is no "present" data.
-      }
-    }
-  });
-
   slides.i0 = slide({
      name : "i0",
      start: function() {
@@ -26,44 +8,101 @@ function make_slides(f) {
      }
   });
 
+  slides.calibration = slide({
+    name : "calibration",
+    ratio : 1,
+    slidewidth : 800,
+    fastshrink : function(){
+      if (this.ratio > .1){
+        this.ratio = this.ratio - .1;
+      }
+      $(".imgcalibrate").attr("width", 350 * this.ratio);
+      $(".imgcalibrate").attr("height", 223 * this.ratio);
+    },
+    shrink : function(){
+      if (this.ratio > .01){
+        this.ratio = this.ratio - .01;
+      }
+      $(".imgcalibrate").attr("width", 350 * this.ratio);
+      $(".imgcalibrate").attr("height", 223 * this.ratio);
+    },
+    enlarge : function(){
+      this.ratio = this.ratio + .01;
+
+      //adjust slide width to try to ensure two images
+      // are on the same line
+      if (700 * this.ratio + 100 > this.slidewidth){
+        this.slidewidth = 700 * this.ratio + 105;
+        $(".slide").css("width", this.slidewidth);
+      }
+
+      $(".imgcalibrate").attr("width", 350 * this.ratio);
+      $(".imgcalibrate").attr("height", 223 * this.ratio);
+    },
+    fastenlarge : function(){
+      this.ratio = this.ratio + .1;
+
+      //adjust slide width to try to ensure two images
+      // are on the same line
+      if (700 * this.ratio + 100 > this.slidewidth){
+        this.slidewidth = 700 * this.ratio + 105;
+        $(".slide").css("width", this.slidewidth);
+      }
+
+      $(".imgcalibrate").attr("width", 350 * this.ratio);
+      $(".imgcalibrate").attr("height", 223 * this.ratio);
+    },
+
+    narrow : function(){
+      if (this.slidewidth > 10){
+        this.slidewidth = this.slidewidth - 10;
+      }
+      $(".slide").css("width", this.slidewidth);
+    },
+    fastnarrow : function(){
+      if (this.slidewidth > 50){
+        this.slidewidth = this.slidewidth - 50;
+      }
+      $(".slide").css("width", this.slidewidth);
+    },
+    widen : function(){
+      this.slidewidth = this.slidewidth + 10;
+      $(".slide").css("width", this.slidewidth);
+    },
+    fastwiden : function(){
+      this.slidewidth = this.slidewidth + 50;
+      $(".slide").css("width", this.slidewidth);
+    },
+
+    button : function() {
+      exp.system.ratio = this.ratio;
+      exp.system.slidewidth = this.slidewidth;
+      $(".imgcrit").attr("width", 350 * this.ratio);
+      $(".imgcrit").attr("height", 350 * this.ratio);
+      exp.go(); //use exp.go() if and only if there is no "present" data.
+    }
+  });
+
+
   slides.instructions = slide({
     name : "instructions",
+
     button : function() {
       exp.go(); //use exp.go() if and only if there is no "present" data.
     }
   });
 
-  // slides.single_trial = slide({
-  //   name: "single_trial",
-  //   start: function() {
-  //     $(".err").hide();
-  //     $(".display_condition").html("You are in " + exp.condition + ".");
-  //   },
-  //   button : function() {
-  //     response = $("#text_response").val();
-  //     if (response.length == 0) {
-  //       $(".err").show();
-  //     } else {
-  //       exp.data_trials.push({
-  //         "trial_type" : "single_trial",
-  //         "response" : response
-  //       });
-  //       exp.go(); //make sure this is at the *end*, after you log your data
-  //     }
-  //   },
-  // });
 
-  slides.one_slider = slide({
-    name : "one_slider",
-
+  slides.practice = slide({
+    name : "practice",
+    start: function() {
+     exp.trialStartT = Date.now();
+   },
     /* trial information for this block
      (the variable 'stim' will change between each of these values,
       and for each of these, present_handle will be run.) */
-    present : [
-      {subject: "dog", object: "ball"},
-      {subject: "cat", object: "windowsill"},
-      {subject: "bird", object: "shiny object"},
-    ],
+    present : [{noun: "cup", adjective: "blue", fileinit: "cupBlue",
+       range:[[1, 9]], comp: "black", correct: "pos"}],
 
     //this gets run only at the beginning of the block
     present_handle : function(stim) {
@@ -71,17 +110,77 @@ function make_slides(f) {
 
       this.stim = stim; //I like to store this information in the slide so I can record it later.
 
+      this.numAttempts = 0;
 
-      // $(".prompt").html(stim.subject + "s like " + stim.object + "s.");
-      this.init_sliders();
-      exp.sliderPost = null; //erase current slider value
+      //Set images to loading and unselect radio buttons
+      $("#practice-imgleft").attr("src", "../stimuli/loading.jpg");
+      $("#practice-imgright").attr("src", "../stimuli/loading.jpg");
+
+      $('input[name=practice-choice]').prop('checked', false);
+
+      // determine which pair of degrees to use
+      // here low degrees mean low wrt the current adjective
+      this.stim.dpair = _.sample(stim.range);
+      this.stim.dlow = this.stim.dpair[0];
+      this.stim.dhigh = this.stim.dpair[1];
+
+      // randomize the locations of the degrees
+      this.stim.lowhigh = _.shuffle(["left", "right"]);
+
+      // the location of image of the lower degree
+      this.stim.loclow = this.stim.lowhigh[0];
+
+      // the location of image of the higher degree
+      this.stim.lochigh = this.stim.lowhigh[1];
+
+      // update images: content and border
+
+      $("#practice-img"+this.stim.loclow).attr("src", "../stimuli/" + stim.fileinit + this.stim.dlow + ".png");
+      $("#practice-img"+this.stim.lochigh).attr("src", "../stimuli/" + stim.fileinit + this.stim.dhigh + ".png");
+
+      $("#practice-img"+this.stim.loclow).css("border", "dotted black");
+      $("#practice-img"+this.stim.lochigh).css("border", "solid red");
+
+      $("#practice-request").html("Please click on the ____ " + stim.noun + ".");
+
+      // randomize the locations of the two words
+      poscomp = _.shuffle(["left", "right"]);
+
+      // the location of the positive form: left or right
+      locpos = poscomp[0];
+
+      // the location of the comparative form: left or right
+      loccomp = poscomp[1];
+
+      // update words and radio button values
+      $("#practice-choice" + locpos).attr("value", "pos");
+      $("#practice-choice" + loccomp).attr("value", "comp");
+
+      $(".practice-text" + locpos).html(stim.adjective);
+      $(".practice-text" + loccomp).html(stim.comp);
+
+      this.stim.locpos = locpos;
+
     },
 
     button : function() {
-      if (exp.sliderPost == null) {
-        $(".err").show();
-      } else {
+      exp.practiceChoice = $('input[name=practice-choice]:checked').val();
+
+      this.numAttempts = this.numAttempts + 1;
+      if (this.numAttempts == 1) {
+        this.firstResponse = exp.practiceChoice;
+      }
+
+      if (exp.practiceChoice == null) {
+        $("#noselection").show();
+      }
+      else if (exp.practiceChoice != this.stim.correct){
+        $("#incorrectpractice").show();
+      }
+      else {
         this.log_responses();
+
+        exp.trialStartT = Date.now();
 
         /* use _stream.apply(this); if and only if there is
         "present" data. (and only *after* responses are logged) */
@@ -89,192 +188,157 @@ function make_slides(f) {
       }
     },
 
-    init_sliders : function() {
-      utils.make_slider("#single_slider", function(event, ui) {
-        exp.sliderPost = ui.value;
-      });
-    },
 
     log_responses : function() {
-      exp.data_trials.push({
-        "trial_type" : "one_slider",
-        "response" : exp.sliderPost
+      exp.catch_trials.push({
+        "noun" : this.stim.noun,
+        "adjective" : this.stim.adjective,
+        "competitor" : this.stim.comp,
+        "fileinit" : this.stim.fileinit,
+        "dlow" : this.stim.dlow,
+        "dhigh" : this.stim.dhigh,
+        "firstresponse" : this.firstResponse,
+        "numattempts" : this.numAttempts,
+        // "reasonother": $("#practice-other").val(),
+        "RT" : Date.now() - exp.trialStartT,
+        "loclow" : this.stim.loclow,
+        "locpos" : this.stim.locpos
       });
     }
   });
+
+
+  slides.pretask = slide({
+    name : "pretask",
+
+    button : function() {
+      exp.go(); //use exp.go() if and only if there is no "present" data.
+    }
+  });
   //
-  // slides.multi_slider = slide({
-  //   name : "multi_slider",
-  //   present : _.shuffle([
-  //     {"critter":"Wugs", "property":"fur"},
-  //     {"critter":"Blicks", "property":"fur"}
-  //   ]),
-  //   present_handle : function(stim) {
-  //     $(".err").hide();
-  //     this.stim = stim; //FRED: allows you to access stim in helpers
-  //
-  //     this.sentence_types = _.shuffle(["generic", "negation", "always", "sometimes", "usually"]);
-  //     var sentences = {
-  //       "generic": stim.critter + " have " + stim.property + ".",
-  //       "negation": stim.critter + " do not have " + stim.property + ".",
-  //       "always": stim.critter + " always have " + stim.property + ".",
-  //       "sometimes": stim.critter + " sometimes have " + stim.property + ".",
-  //       "usually": stim.critter + " usually have " + stim.property + "."
-  //     };
-  //
-  //     this.n_sliders = this.sentence_types.length;
-  //     $(".slider_row").remove();
-  //     for (var i=0; i<this.n_sliders; i++) {
-  //       var sentence_type = this.sentence_types[i];
-  //       var sentence = sentences[sentence_type];
-  //       $("#multi_slider_table").append('<tr class="slider_row"><td class="slider_target" id="sentence' + i + '">' + sentence + '</td><td colspan="2"><div id="slider' + i + '" class="slider">-------[ ]--------</div></td></tr>');
-  //       utils.match_row_height("#multi_slider_table", ".slider_target");
-  //     }
-  //
-  //     this.init_sliders(this.sentence_types);
-  //     exp.sliderPost = [];
-  //   },
-  //
-  //   button : function() {
-  //     if (exp.sliderPost.length < this.n_sliders) {
-  //       $(".err").show();
-  //     } else {
-  //       this.log_responses();
-  //       _stream.apply(this); //use _stream.apply(this); if and only if there is "present" data.
-  //     }
-  //   },
-  //
-  //   init_sliders : function(sentence_types) {
-  //     for (var i=0; i<sentence_types.length; i++) {
-  //       var sentence_type = sentence_types[i];
-  //       utils.make_slider("#slider" + i, this.make_slider_callback(i));
-  //     }
-  //   },
-  //   make_slider_callback : function(i) {
-  //     return function(event, ui) {
-  //       exp.sliderPost[i] = ui.value;
-  //     };
-  //   },
-  //   log_responses : function() {
-  //     for (var i=0; i<this.sentence_types.length; i++) {
-  //       var sentence_type = this.sentence_types[i];
-  //       exp.data_trials.push({
-  //         "trial_type" : "multi_slider",
-  //         "sentence_type" : sentence_type,
-  //         "response" : exp.sliderPost[i]
-  //       });
-  //     }
-  //   },
-  // });
-  //
-  // slides.vertical_sliders = slide({
-  //   name : "vertical_sliders",
-  //   present : _.shuffle([
-  //     {
-  //       "bins" : [
-  //         {
-  //           "min" : 0,
-  //           "max" : 10
-  //         },
-  //         {
-  //           "min" : 10,
-  //           "max" : 20
-  //         },
-  //         {
-  //           "min" : 20,
-  //           "max" : 30
-  //         },
-  //         {
-  //           "min" : 30,
-  //           "max" : 40
-  //         },
-  //         {
-  //           "min" : 40,
-  //           "max" : 50
-  //         },
-  //         {
-  //           "min" : 50,
-  //           "max" : 60
-  //         }
-  //       ],
-  //       "question": "How tall is tall?"
-  //     }
-  //   ]),
-  //   present_handle : function(stim) {
-  //     $(".err").hide();
-  //     this.stim = stim;
-  //
-  //     $("#vertical_question").html(stim.question);
-  //
-  //     $("#sliders").empty();
-  //     $("#bin_labels").empty();
-  //
-  //     $("#sliders").append('<td> \
-  //           <div id="slider_endpoint_labels"> \
-  //             <div class="top">likely</div> \
-  //             <div class="bottom">unlikely</div>\
-  //           </div>\
-  //         </td>')
-  //     $("#bin_labels").append('<td></td>')
-  //
-  //     this.n_sliders = stim.bins.length;
-  //     for (var i=0; i<stim.bins.length; i++) {
-  //       $("#sliders").append("<td><div id='vslider" + i + "' class='vertical_slider'>|</div></td>");
-  //       $("#bin_labels").append("<td class='bin_label'>" + stim.bins[i].min + " - " + stim.bins[i].max + "</td>");
-  //     }
-  //
-  //     this.init_sliders(stim);
-  //     exp.sliderPost = [];
-  //   },
-  //
-  //   button : function() {
-  //     if (exp.sliderPost.length < this.n_sliders) {
-  //       $(".err").show();
-  //     } else {
-  //       this.log_responses();
-  //       _stream.apply(this); //use _stream.apply(this); if and only if there is "present" data.
-  //     }
-  //   },
-  //
-  //   init_sliders : function(stim) {
-  //     for (var i=0; i<stim.bins.length; i++) {
-  //       utils.make_slider("#vslider" + i, this.make_slider_callback(i), "vertical");
-  //     }
-  //   },
-  //   make_slider_callback : function(i) {
-  //     return function(event, ui) {
-  //       exp.sliderPost[i] = ui.value;
-  //     };
-  //   },
-  //   log_responses : function() {
-  //     for (var i=0; i<this.stim.bins.length; i++) {
-  //       exp.data_trials.push({
-  //         "trial_type" : "vertical_slider",
-  //         "question" : this.stim.question,
-  //         "response" : exp.sliderPost[i],
-  //         "min" : this.stim.bins[i].min,
-  //         "max" : this.stim.bins[i].max
-  //       });
-  //     }
-  //   },
-  // });
+  // Critical trials
+
+  slides.critical = slide({
+    name : "critical",
+    start: function() {
+     exp.trialStartT = Date.now();
+   },
+    /* trial information for this block
+     (the variable 'stim' will change between each of these values,
+      and for each of these, present_handle will be run.) */
+    present : allStims,
+
+    //this gets run only at the beginning of the block
+    present_handle : function(stim) {
+      $(".err").hide();
+
+      this.stim = stim; //I like to store this information in the slide so I can record it later.
+
+      //Set images to loading and unselect radio buttons
+      $("#critical-imgleft").attr("src", "../stimuli/loading.jpg");
+      $("#critical-imgright").attr("src", "../stimuli/loading.jpg");
+
+      $('input[name=critical-choice]').prop('checked', false);
+
+      // determine which pair of degrees to use
+      // here low degrees mean low wrt the current adjective
+      this.stim.dpair = _.sample(stim.range);
+      this.stim.dlow = this.stim.dpair[0];
+      this.stim.dhigh = this.stim.dpair[1];
+
+      // randomize the locations of the degrees
+      this.stim.lowhigh = _.shuffle(["left", "right"]);
+
+      // the location of image of the lower degree
+      this.stim.loclow = this.stim.lowhigh[0];
+
+      // the location of image of the higher degree
+      this.stim.lochigh = this.stim.lowhigh[1];
+
+      // update images: content and border
+
+      $("#critical-img"+this.stim.loclow).attr("src", "../stimuli/" + stim.fileinit + this.stim.dlow + ".png");
+      $("#critical-img"+this.stim.lochigh).attr("src", "../stimuli/" + stim.fileinit + this.stim.dhigh + ".png");
+
+      $("#critical-img"+this.stim.loclow).css("border", "dotted black");
+      $("#critical-img"+this.stim.lochigh).css("border", "solid red");
+
+      $("#critical-request").html("Please click on the ____ " + stim.noun + ".");
+
+      // randomize the locations of the two words
+      poscomp = _.shuffle(["left", "right"]);
+
+      // the location of the positive form: left or right
+      locpos = poscomp[0];
+
+      // the location of the comparative form: left or right
+      loccomp = poscomp[1];
+
+      // update words and radio button values
+      $("#critical-choice" + locpos).attr("value", "pos");
+      $("#critical-choice" + loccomp).attr("value", "comp");
+
+      $(".critical-text" + locpos).html(stim.adjective);
+      $(".critical-text" + loccomp).html(stim.comp);
+
+      this.stim.locpos = locpos;
+
+    },
+
+    button : function() {
+      exp.criticalChoice = $('input[name=critical-choice]:checked').val();
+
+      if (exp.criticalChoice == null) {
+        $(".err").show();
+      } else {
+        this.log_responses();
+        exp.trialStartT = Date.now();
+
+        /* use _stream.apply(this); if and only if there is
+        "present" data. (and only *after* responses are logged) */
+        _stream.apply(this);
+      }
+    },
+
+
+    log_responses : function() {
+      exp.data_trials.push({
+        "noun" : this.stim.noun,
+        "adjective" : this.stim.adjective,
+        "comparative" : this.stim.comp,
+        "fileinit" : this.stim.fileinit,
+        "dlow" : this.stim.dlow,
+        "dhigh" : this.stim.dhigh,
+        "response" : exp.criticalChoice,
+        "RT" : Date.now() - exp.trialStartT,
+        "loclow" : this.stim.loclow,
+        "locpos" : this.stim.locpos
+      });
+    }
+  });
 
   slides.subj_info =  slide({
     name : "subj_info",
     submit : function(e){
       //if (e.preventDefault) e.preventDefault(); // I don't know what this means.
       exp.subj_data = {
+        device : $("#device").val(),
         language : $("#language").val(),
         enjoyment : $("#enjoyment").val(),
         asses : $('input[name="assess"]:checked').val(),
-        age : $("#age").val(),
-        gender : $("#gender").val(),
-        education : $("#education").val(),
+        // age : $("#age").val(),
+        // gender : $("#gender").val(),
+        // education : $("#education").val(),
         comments : $("#comments").val(),
         problems: $("#problems").val(),
         fairprice: $("#fairprice").val()
       };
-      exp.go(); //use exp.go() if and only if there is no "present" data.
+      if (exp.subj_data.language == ""){
+        $(".err").show();
+      }
+      else {
+        exp.go(); //use exp.go() if and only if there is no "present" data.
+      }
     }
   });
 
@@ -310,7 +374,9 @@ function init() {
       screenUW: exp.width
     };
   //blocks of the experiment:
-  exp.structure=["i0", "consent", "instructions", "one_slider", 'subj_info', 'thanks'];
+  exp.structure=["i0", "calibration",
+  "instructions", "practice", "pretask",
+  "critical", 'subj_info', 'thanks'];
 
   exp.data_trials = [];
   //make corresponding slides:
@@ -320,6 +386,8 @@ function init() {
                     //relies on structure and slides being defined
 
   $('.slide').hide(); //hide everything
+
+
 
   //make sure turkers have accepted HIT (or you're not in mturk)
   $("#start_button").click(function() {
