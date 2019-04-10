998e9bc7ecac89677e40852e2d12b168f8a9412c
Ciyang <qciyang@gmail.com>
2019-04-10 08:02:29 -0700

    comprehension pilot v2

---
+++ js/stimsCompPilot2.js
@@ -0,0 +1,10 @@
+allStims = [
+  {noun: "pencil", adjective: "long", fileinit: "pencilLong", range:[[1,4], [3,4]]},
+  {noun: "glass", adjective: "tall", fileinit: "glassTall", range:[[1,4], [3,4]]},
+  {noun: "book", adjective: "thick", fileinit: "bookThick", range:[[1,4], [3,4]]},
+  {noun: "ladder", adjective: "wide", fileinit: "ladderWide", range:[[1,4], [3,4]]},
+  {noun: "glass", adjective: "full", fileinit: "glassFull", range:[[8,9], [4,5]]},
+  {noun: "bottle", adjective: "full", fileinit: "bottleFull", range:[[8,9], [4,6]]}
+];
+
+allStims = _.shuffle(allStims);
