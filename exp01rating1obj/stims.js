12d496282cf17336cc4fdb89302a2b3fd4fb96c8
Ciyang <qciyang@gmail.com>
2019-04-11 07:56:18 -0700

    new norming1obj

---
+++ exp01rating1obj/stims.js
@@ -0,0 +1,14 @@
+// This is the stimuli file for
+// agreement1obj.html
+// (Add more descriptions if needed)
+
+allStims = [
+  {noun: "pencil", adjective: "long", fileinit: "pencilLong", range:[100, 160, 400]},
+  {noun: "glass", adjective: "tall", fileinit: "glassTall", range:[180, 240, 360]},
+  {noun: "book", adjective: "thick", fileinit: "bookThick", range:[30, 40, 70]},
+  {noun: "glass", adjective: "full", fileinit: "glassFull", range:[180, 300, 480]},
+  {noun: "bottle", adjective: "full", fileinit: "bottleFull", range:[180, 300, 400]},
+  {noun: "straw", adjective: "straight", fileinit: "strawStraight", range:[7,8,9]},
+];
+
+allStims = _.shuffle(allStims);
