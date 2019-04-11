ccab1314ab3f9a7b13af045ab74471465f70e61d
Ciyang <qciyang@gmail.com>
2019-04-11 11:04:09 -0700

    norming1objAgreement batch2

---
+++ exp01rating1obj/stimsBatch2.js
@@ -0,0 +1,14 @@
+// This is the stimuli file for
+// agreement1objBatch2.html
+// (Add more descriptions if needed)
+
+allStims = [
+  {noun: "pencil", adjective: "long", fileinit: "pencilLong", range:[140, 420]},
+  {noun: "glass", adjective: "tall", fileinit: "glassTall", range:[220, 420]},
+  {noun: "book", adjective: "thick", fileinit: "bookThick", range:[25, 80]},
+  {noun: "glass", adjective: "full", fileinit: "glassFull", range:[360, 420]},
+  {noun: "bottle", adjective: "full", fileinit: "bottleFull", range:[340, 380]},
+  {noun: "straw", adjective: "straight", fileinit: "strawStraight", range:[3,5]},
+];
+
+allStims = _.shuffle(allStims);
