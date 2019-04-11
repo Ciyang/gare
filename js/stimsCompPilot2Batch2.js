eac6e1a26ef6a8f2d49716fc79bf82f18e9c424a
Ciyang <qciyang@gmail.com>
2019-04-11 11:42:40 -0700

    comprehensionPilot2Batch2

---
+++ js/stimsCompPilot2Batch2.js
@@ -0,0 +1,10 @@
+allStims = [
+  {noun: "pencil", adjective: "long", fileinit: "pencilLong", range:[[140,160], [400,420]]},
+  {noun: "glass", adjective: "tall", fileinit: "glassTall", range:[[180,240], [220,240]]},
+  {noun: "book", adjective: "thick", fileinit: "bookThick", range:[[25,30], [70,80]]},
+  {noun: "straw", adjective: "straight", fileinit: "strawStraight", range:[[3,8], [5,8]]},
+  {noun: "glass", adjective: "full", fileinit: "glassFull", range:[[180,360], [420,500]]},
+  {noun: "bottle", adjective: "full", fileinit: "bottleFull", range:[[180,340], [380,420]]}
+];
+
+allStims = _.shuffle(allStims);
