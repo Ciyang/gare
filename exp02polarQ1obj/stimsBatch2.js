f18d0632e598673c3a082433f7e1481ce29fba94
Ciyang <qciyang@gmail.com>
2019-04-19 10:00:40 -0700

    norming 1 object polar question binary batch 2

---
+++ exp02polarQ1obj/stimsBatch2.js
@@ -0,0 +1,14 @@
+// This is the stimuli file for
+// polarQ1obj.html
+// (Add more descriptions if needed)
+
+allStims = [
+  {noun: "pencil", adjective: "long", fileinit: "pencilLong", range:[420]},
+  {noun: "glass", adjective: "tall", fileinit: "glassTall", range:[240]},
+  {noun: "book", adjective: "thick", fileinit: "bookThick", range:[40]},
+  {noun: "paintbrush", adjective: "wide", fileinit: "brushWide", range:[60]},
+  {noun: "bottle", adjective: "full", fileinit: "bottleFull", range:[400]},
+  {noun: "straw", adjective: "straight", fileinit: "strawStraight", range:[9]},
+];
+
+allStims = _.shuffle(allStims);
