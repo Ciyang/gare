bf719edbe1048ebe7e2ff8c434e5d51c90f819d5
Ciyang <qciyang@gmail.com>
2019-04-23 10:25:14 -0700

    2obj polarQ batch3 (some crisp judgments)

---
+++ exp03polarQ2obj/stimsBatch3.js
@@ -0,0 +1,20 @@
+allStims = [
+  {noun: "pencil", adjective: "long", fileinit: "pencilLong", range:[[140,160]], targetRange:["dhigh"]},
+  {noun: "glass", adjective: "tall", fileinit: "glassTall", range:[[220,240]], targetRange:["dhigh"]},
+  {noun: "book", adjective: "thick", fileinit: "bookThick", range:[[40,50]], targetRange:["dhigh"]},
+  {noun: "paintbrush", adjective: "wide", fileinit: "brushWide", range:[[90,120]], targetRange:["dhigh"]},
+  {noun: "bottle", adjective: "full", fileinit: "bottleFull", range:[[340,400]], targetRange:["dhigh"]},
+  {noun: "bottle", adjective: "empty", fileinit: "bottleFull", range:[[140,80]], targetRange:["dhigh"]},
+  {noun: "straw", adjective: "straight", fileinit: "strawStraight", range:[[7,8]], targetRange:["dhigh"]},
+  {noun: "feather", adjective: "dark", fileinit: "featherDark", range:[[14,37]], targetRange:["dhigh"]},
+  {noun: "pencil", adjective: "long", fileinit: "pencilLong", range:[[140,160]], targetRange:["dlow"]},
+  {noun: "glass", adjective: "tall", fileinit: "glassTall", range:[[220,240]], targetRange:["dlow"]},
+  {noun: "book", adjective: "thick", fileinit: "bookThick", range:[[40,50]], targetRange:["dlow"]},
+  {noun: "paintbrush", adjective: "wide", fileinit: "brushWide", range:[[90,120]], targetRange:["dlow"]},
+  {noun: "bottle", adjective: "full", fileinit: "bottleFull", range:[[340,400]], targetRange:["dlow"]},
+  {noun: "bottle", adjective: "empty", fileinit: "bottleFull", range:[[140,80]], targetRange:["dlow"]},
+  {noun: "straw", adjective: "straight", fileinit: "strawStraight", range:[[7,8]], targetRange:["dlow"]},
+  {noun: "feather", adjective: "dark", fileinit: "featherDark", range:[[14,37]], targetRange:["dlow"]}
+];
+
+allStims = _.shuffle(allStims);
