1d29cf2ded3415fe243c69b1a6edc2d01f4ebd75
Ciyang <qciyang@gmail.com>
2019-04-23 08:47:23 -0700

    polarQ 2obj context only dhigh

---
+++ exp03polarQ2obj/stimsBatch2.js
@@ -0,0 +1,12 @@
+allStims = [
+  {noun: "pencil", adjective: "long", fileinit: "pencilLong", range:[[100,160]], targetRange:["dhigh"]},
+  {noun: "glass", adjective: "tall", fileinit: "glassTall", range:[[180,240]], targetRange:["dhigh"]},
+  {noun: "book", adjective: "thick", fileinit: "bookThick", range:[[25,40]], targetRange:["dhigh"]},
+  {noun: "paintbrush", adjective: "wide", fileinit: "brushWide", range:[[60,80]], targetRange:["dhigh"]},
+  {noun: "bottle", adjective: "full", fileinit: "bottleFull", range:[[180,340]], targetRange:["dhigh"]},
+  {noun: "bottle", adjective: "empty", fileinit: "bottleFull", range:[[80,20]], targetRange:["dhigh"]},
+  {noun: "straw", adjective: "straight", fileinit: "strawStraight", range:[[3,8]], targetRange:["dhigh"]},
+  {noun: "feather", adjective: "dark", fileinit: "featherDark", range:[[65,75]], targetRange:["dhigh"]}
+];
+
+allStims = _.shuffle(allStims);
