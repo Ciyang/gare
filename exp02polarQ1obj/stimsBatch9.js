c182dd52d33faed99b787a1eb87672f9f947c076
Ciyang <qciyang@gmail.com>
2019-04-29 08:51:56 -0700

    norming 1obj polarQ batch9

---
+++ exp02polarQ1obj/stimsBatch9.js
@@ -0,0 +1,16 @@
+// This is the stimuli file for
+// polarQ1objBatch9.html
+// (Add more descriptions if needed)
+
+allStims = [
+  {noun: "pencil", adjective: "long", fileinit: "pencilLong", range:[180,300]},
+  {noun: "glass", adjective: "tall", fileinit: "glassTall", range:[270,290,330]},
+  {noun: "book", adjective: "thick", fileinit: "bookThick", range:[55,65]},
+  {noun: "paintbrush", adjective: "wide", fileinit: "brushWide", range:[20,30,40,160]},
+  {noun: "bottle", adjective: "full", fileinit: "bottleFull", range:[330,370]},
+  {noun: "bottle", adjective: "empty", fileinit: "bottleFull", range:[30,110]},
+  {noun: "feather", adjective: "dark", fileinit: "featherDark", range:[30, 60]},
+  {noun: "straw", adjective: "straight", fileinit: "strawStraight", range:[176,178]},
+];
+
+allStims = _.shuffle(allStims);
